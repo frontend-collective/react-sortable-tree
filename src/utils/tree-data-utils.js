bba0463ab9d32d931fc78e6c14c924c139667ab2
fritz-c <fritz-c@users.noreply.github.com>
2016-08-02 15:35:28 +0900

    Add a couple treeData util functions and their tests

--- src/utils/tree-data-utils.js
+++ src/utils/tree-data-utils.js
@@ -1,3 +1,74 @@
-export function hello() {
-    return 'world';
+/**
+ * Count all the visible (expanded) descendants in the tree data.
+ *
+ * @param {!Object[]} data - Tree data
+ *
+ * @return {number} count
+ */
+export function getVisibleNodeCount(data) {
+    const traverse = (node) => {
+        if (!node.children || node.expanded !== true) {
+            return 1;
+        }
+
+        return 1 + node.children.reduce((total, currentNode) => (total + traverse(currentNode)), 0);
+    };
+
+    return data.reduce((total, currentNode) => total + traverse(currentNode), 0);
+}
+
+/**
+ * Get the <targetIndex>th visible node in the tree data.
+ *
+ * @param {!Object[]} data - Tree data
+ * @param {!number} targetIndex - The index of the node to search for
+ *
+ * @return {Object|null} node - The node at targetIndex, or null if not found
+ */
+export function getVisibleNodeAtIndex(data, targetIndex) {
+    if (!data || data.length < 1) {
+        return null;
+    }
+
+    // Performs a depth-first traversal over all of the node descendants,
+    // incrementing currentIndex by 1 for each
+    const traverse = (node, currentIndex) => {
+        // Return target node when found
+        if (currentIndex === targetIndex) {
+            return { node, currentIndex };
+        }
+
+        // Add one and continue for nodes with no children or hidden children
+        if (!node.children || node.expanded !== true) {
+            return { currentIndex: currentIndex + 1 };
+        }
+
+        // Iterate over each child and their ancestors and return the
+        // target node if childIndex reaches the targetIndex
+        let childIndex = currentIndex + 1;
+        for (let i = 0; i < node.children.length; i++) {
+            const result = traverse(node.children[i], childIndex);
+            if (result.node) {
+                return result;
+            }
+
+            childIndex = result.currentIndex;
+        }
+
+        // If the target node is not found, return the farthest traversed index
+        return { currentIndex: childIndex };
+    };
+
+    // Kick off the search on the top level of the data array
+    let currentIndex = 0;
+    for (let i = 0; i < data.length; i++) {
+        const result = traverse(data[i], currentIndex);
+        if (result.node) {
+            return result.node;
+        }
+
+        currentIndex = result.currentIndex;
+    }
+
+    return null;
 }
--- src/utils/tree-data-utils.test.js
+++ src/utils/tree-data-utils.test.js
@@ -1,7 +1,198 @@
-import { hello } from './tree-data-utils';
+import {
+    getVisibleNodeCount,
+    getVisibleNodeAtIndex,
+} from './tree-data-utils';
 
-describe('magic', () => {
-    it('should happen', () => {
-        expect(hello()).toEqual('world');
+describe('getVisibleNodeCount', () => {
+    it('should handle flat data', () => {
+        expect(getVisibleNodeCount([
+            {},
+            {},
+        ])).toEqual(2);
+    });
+
+    it('should handle hidden nested data', () => {
+        expect(getVisibleNodeCount([
+            {
+                children: [
+                    {
+                        children: [
+                            {},
+                            {},
+                        ],
+                    },
+                    {
+                        children: [
+                            {},
+                        ],
+                    },
+                ],
+            },
+            {},
+        ])).toEqual(2);
+    });
+
+    it('should handle partially expanded nested data', () => {
+        expect(getVisibleNodeCount([
+            {
+                expanded: true,
+                children: [
+                    {
+                        expanded: true,
+                        children: [
+                            {},
+                            {},
+                        ],
+                    },
+                    {
+                        children: [
+                            {},
+                        ],
+                    },
+                ],
+            },
+            {},
+        ])).toEqual(6);
+    });
+
+    it('should handle fully expanded nested data', () => {
+        expect(getVisibleNodeCount([
+            {
+                expanded: true,
+                children: [
+                    {
+                        expanded: true,
+                        children: [
+                            {},
+                            {},
+                        ],
+                    },
+                    {
+                        expanded: true,
+                        children: [
+                            {},
+                        ],
+                    },
+                ],
+            },
+            {},
+        ])).toEqual(7);
+    });
+});
+
+describe('getVisibleNodeAtIndex', () => {
+    it('should handle empty data', () => {
+        expect(getVisibleNodeAtIndex([], 1)).toEqual(null);
+        expect(getVisibleNodeAtIndex(null, 1)).toEqual(null);
+        expect(getVisibleNodeAtIndex(undefined, 1)).toEqual(null);
+    });
+
+    it('should handle flat data', () => {
+        expect(getVisibleNodeAtIndex([ { id: 0 } ], 0).id).toEqual(0);
+        expect(getVisibleNodeAtIndex([ { id: 0 }, { id: 1 } ], 1).id).toEqual(1);
+    });
+
+    it('should handle hidden nested data', () => {
+        expect(getVisibleNodeAtIndex([
+            {
+                id: 0,
+                children: [
+                    {
+                        id: 1,
+                        children: [
+                            { id: 2 },
+                            { id: 3 },
+                        ],
+                    },
+                    {
+                        id: 4,
+                        children: [
+                            { id: 5 },
+                        ],
+                    },
+                ],
+            },
+            { id: 6 },
+        ], 1).id).toEqual(6);
+    });
+
+    it('should handle partially expanded nested data', () => {
+        expect(getVisibleNodeAtIndex([
+            {
+                expanded: true,
+                id: 0,
+                children: [
+                    {
+                        id: 1,
+                        children: [
+                            { id: 2 },
+                            { id: 3 },
+                        ],
+                    },
+                    {
+                        expanded: true,
+                        id: 4,
+                        children: [
+                            { id: 5 },
+                        ],
+                    },
+                ],
+            },
+            { id: 6 },
+        ], 3).id).toEqual(5);
+    });
+
+    it('should handle fully expanded nested data', () => {
+        expect(getVisibleNodeAtIndex([
+            {
+                expanded: true,
+                id: 0,
+                children: [
+                    {
+                        expanded: true,
+                        id: 1,
+                        children: [
+                            { id: 2 },
+                            { id: 3 },
+                        ],
+                    },
+                    {
+                        expanded: true,
+                        id: 4,
+                        children: [
+                            { id: 5 },
+                        ],
+                    },
+                ],
+            },
+            { id: 6 },
+        ], 5).id).toEqual(5);
+    });
+
+    it('should handle an index that is larger than the data', () => {
+        expect(getVisibleNodeAtIndex([
+            {
+                expanded: true,
+                id: 0,
+                children: [
+                    {
+                        expanded: true,
+                        id: 1,
+                        children: [
+                            { id: 2 },
+                            { id: 3 },
+                        ],
+                    },
+                    {
+                        expanded: true,
+                        id: 4,
+                        children: [
+                            { id: 5 },
+                        ],
+                    },
+                ],
+            },
+            { id: 6 },
+        ], 7)).toEqual(null);
     });
 });
