b3300e0de4d669125f7f4ea6f783e6edc002d847
fritz-c <fritz-c@users.noreply.github.com>
2016-09-15 14:18:01 +0900

    Add style info to the top of the head, making css overrides easier

--- webpack.config.umd.babel.js
+++ webpack.config.umd.babel.js
@@ -55,7 +55,7 @@ module.exports = {
             {
                 test: /\.scss$/,
                 loaders: [
-                    'style-loader',
+                    'style-loader?insertAt=top',
                     'css-loader?modules&importLoaders=1&localIdentName=rst__[local]',
                     'postcss-loader',
                     'sass-loader',
@@ -65,7 +65,7 @@ module.exports = {
             {
                 test: /\.css$/,
                 loaders: [
-                    'style-loader',
+                    'style-loader?insertAt=top',
                     'css-loader',
                     'postcss-loader',
                 ],
