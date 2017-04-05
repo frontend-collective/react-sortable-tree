05c09d32f57bd8cc67e1af13dd7437523347fc78
noursammour <nour.sammour@hotmail.com>
2017-04-06 00:55:18 +0300

    export external node decorator

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import SortableTree, { SortableTreeWithoutDndContext } from './react-sortable-tree';
+import SortableTree, { SortableTreeWithoutDndContext, ExternalItemWrapper } from './react-sortable-tree';
 
 export * from './utils/default-handlers';
 export * from './utils/tree-data-utils';
@@ -8,3 +8,5 @@ export default SortableTree;
 // for when component is used with other components using react-dnd.
 // see: https://github.com/gaearon/react-dnd/issues/186
 export { SortableTreeWithoutDndContext };
+export { ExternalItemWrapper };
+
