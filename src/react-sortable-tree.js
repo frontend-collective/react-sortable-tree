a906f21da438d0f8d28df869c4ffa2e38293b28c
fritz-c <fritz-c@users.noreply.github.com>
2017-09-20 23:37:43 +0900

    fix: add row variables to the callback at the top level

--- src/react-sortable-tree.js
+++ src/react-sortable-tree.js
@@ -558,7 +558,7 @@ class ReactSortableTree extends Component {
             height:
               typeof rowHeight !== 'function'
                 ? rowHeight
-                : rowHeight({ index, row }),
+                : rowHeight({ ...row, index }),
           },
           () => rows[index - 1] || null,
           matchKeys
