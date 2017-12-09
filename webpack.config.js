c8882032a8ca88ba3e666b50efac16cc1a039a1f
fritz-c <fritz-c@users.noreply.github.com>
2017-12-09 13:32:44 +0900

    chore: disable UglifyJs for package builds

--- webpack.config.js
+++ webpack.config.js
@@ -45,16 +45,7 @@ const config = {
   },
   devtool: 'source-map',
   plugins: [
-    new webpack.EnvironmentPlugin({ NODE_ENV: 'development' }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-      mangle: false,
-      beautify: true,
-      comments: true,
-    }),
+    new webpack.EnvironmentPlugin({ NODE_ENV: 'production' }),
   ],
   module: {
     rules: [
@@ -88,7 +79,7 @@ switch (target) {
     ];
     break;
   case 'development':
-    config.devtool = 'eval';
+    config.devtool = 'eval-source-map';
     config.module.rules.push({
       test: /\.(jpe?g|png|gif|ico|svg)$/,
       use: [fileLoader],
