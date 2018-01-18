7841dd90abd688f7876803efb72880b8513f29c7
wuweiweiwu <wuxx1045@umn.edu>
2018-01-17 21:07:14 -0800

    Adding sandbox environment buttons and styling

--- examples/storybooks/index.js
+++ examples/storybooks/index.js
@@ -18,11 +18,19 @@ import TreeDataIOExample from './tree-data-io';
 import TreeToTreeExample from './tree-to-tree';
 import styles from './generic.scss';
 
-const wrapWithSource = (node, src) => (
+const wrapWithSource = (node, src, sandbox) => (
   <div>
     {node}
 
     <br />
+    <a
+      href={`https://codesandbox.io/s/${sandbox}`}
+      target="_top"
+      rel="noopener noreferrer"
+      className={styles.sandboxLink}
+    >
+      VIEW ON SANDBOX →
+    </a>
     <a
       href={`https://github.com/fritz-c/react-sortable-tree/blob/master/examples/storybooks/${src}`}
       target="_top"
@@ -36,35 +44,51 @@ const wrapWithSource = (node, src) => (
 
 storiesOf('Basics', module)
   .add('Minimal implementation', () =>
-    wrapWithSource(<BarebonesExample />, 'barebones.js')
+    wrapWithSource(<BarebonesExample />, 'barebones.js', '181o18oz3j')
   )
   .add('treeData import/export', () =>
-    wrapWithSource(<TreeDataIOExample />, 'tree-data-io.js')
+    wrapWithSource(<TreeDataIOExample />, 'tree-data-io.js', 'm48xkm0w98')
   )
   .add('Add and remove nodes programmatically', () =>
-    wrapWithSource(<AddRemoveExample />, 'add-remove.js')
+    wrapWithSource(<AddRemoveExample />, 'add-remove.js', 'jvqo7683j5')
   )
   .add('Modify nodes', () =>
-    wrapWithSource(<ModifyNodesExample />, 'modify-nodes.js')
+    wrapWithSource(<ModifyNodesExample />, 'modify-nodes.js', '00mkvmo3yv')
   )
-  .add('Prevent drop', () => wrapWithSource(<CanDropExample />, 'can-drop.js'))
-  .add('Search', () => wrapWithSource(<SearchExample />, 'search.js'))
-  .add('Themes', () => wrapWithSource(<ThemesExample />, 'themes.js'))
-  .add('Callbacks', () => wrapWithSource(<CallbacksExample />, 'callbacks.js'));
+  .add('Prevent drop', () =>
+    wrapWithSource(<CanDropExample />, 'can-drop.js', 'ko13xwv1ko')
+  )
+  .add('Search', () =>
+    wrapWithSource(<SearchExample />, 'search.js', 'j7op92ojpv')
+  )
+  .add('Themes', () =>
+    wrapWithSource(<ThemesExample />, 'themes.js', '9zozn02w5r')
+  )
+  .add('Callbacks', () =>
+    wrapWithSource(<CallbacksExample />, 'callbacks.js', '6w61xz92zn')
+  );
 
 storiesOf('Advanced', module)
   .add('Drag from external source', () =>
-    wrapWithSource(<ExternalNodeExample />, 'external-node.js')
+    wrapWithSource(<ExternalNodeExample />, 'external-node.js', 'lyk94v2x7m')
   )
   .add('Touch support (Experimental)', () =>
-    wrapWithSource(<TouchSupportExample />, 'touch-support.js')
+    wrapWithSource(<TouchSupportExample />, 'touch-support.js', 'k5w17opmkr')
   )
   .add('Tree-to-tree dragging', () =>
-    wrapWithSource(<TreeToTreeExample />, 'tree-to-tree.js')
+    wrapWithSource(<TreeToTreeExample />, 'tree-to-tree.js', 'k97nvzmj2r')
   )
   .add('Playing with generateNodeProps', () =>
-    wrapWithSource(<GenerateNodePropsExample />, 'generate-node-props.js')
+    wrapWithSource(
+      <GenerateNodePropsExample />,
+      'generate-node-props.js',
+      'pmkrxm8yp7'
+    )
   )
   .add('Drag out to remove', () =>
-    wrapWithSource(<DragOutToRemoveExample />, 'drag-out-to-remove.js')
+    wrapWithSource(
+      <DragOutToRemoveExample />,
+      'drag-out-to-remove.js',
+      'k371z4v98r'
+    )
   );
