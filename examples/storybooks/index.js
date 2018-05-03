ce2ccfe66ce3f4a33fe325eb75a1f3eb37b86969
Oleksandr Boiko <mega.boich@gmail.com>
2018-05-03 13:28:40 +0200

    Added example of expand lazy loading to storybook advanced section

--- examples/storybooks/index.js
+++ examples/storybooks/index.js
@@ -15,6 +15,7 @@ import ThemesExample from './themes';
 import TouchSupportExample from './touch-support';
 import TreeDataIOExample from './tree-data-io';
 import TreeToTreeExample from './tree-to-tree';
+import ExpandLazyLoadingExample from './expand-lazy-loading';
 import './generic.css';
 
 import { handleClick, SANDBOX_URL } from './sandbox-utils';
@@ -74,4 +75,7 @@ storiesOf('Advanced', module)
   )
   .add('Drag out to remove', () =>
     wrapWithSource(<DragOutToRemoveExample />, 'drag-out-to-remove.js')
+  )
+  .add('Expand with lazy loading of child nodes', () =>
+    wrapWithSource(<ExpandLazyLoadingExample />, 'expand-lazy-loading.js')
   );
