79d2c257ed29c1fa0aa3f819e4f2622c9bc76e4f
Wei-Wei Wu <wuxx1045@umn.edu>
2018-06-01 00:47:16 -0700

    update .babelrc -> .babelrc.js

---
+++ .babelrc.js
@@ -0,0 +1,34 @@
+const env = process.env.NODE_ENV;
+
+if (env === 'commonjs' || env === 'es') {
+  module.exports = {
+    plugins: ['transform-runtime'],
+    presets: [['env', { modules: false }], 'react', 'stage-2'],
+  };
+
+  if (env === 'commonjs') {
+    module.exports.plugins.push('transform-es2015-modules-commonjs');
+  }
+}
+
+if (env === 'rollup') {
+  module.exports = {
+    comments: false,
+    plugins: ['external-helpers'],
+    presets: [['env', { modules: false }], 'react', 'stage-2'],
+  };
+}
+
+if (env === 'development') {
+  module.exports = {
+    presets: ['react', 'stage-2'],
+  };
+}
+
+if (env === 'production') {
+  module.exports = {
+    comments: false,
+    plugins: ['transform-runtime'],
+    presets: ['env', 'react', 'stage-2'],
+  };
+}
