9f1c8c8042b772f9123a638facd9b502d882b2e7
Wei-Wei Wu <wuxx1045@umn.edu>
2018-06-08 00:39:20 -0700

    demo partly done

--- example/src/App.js
+++ example/src/App.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Demo from './Demo';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <Demo />;
   }
 }
 
---
+++ example/src/Demo.js
@@ -0,0 +1,257 @@
+import React, { Component } from 'react';
+import SortableTree, { toggleExpandedForAll } from 'react-sortable-tree';
+
+const maxDepth = 5;
+
+const renderDepthTitle = ({ path }) => `Depth: ${path.length}`;
+
+const alertNodeInfo = ({ node, path, treeIndex }) => {
+  const objectString = Object.keys(node)
+    .map(k => (k === 'children' ? 'children: Array' : `${k}: '${node[k]}'`))
+    .join(',\n   ');
+
+  global.alert(
+    'Info passed to the button generator:\n\n' +
+      `node: {\n   ${objectString}\n},\n` +
+      `path: [${path.join(', ')}],\n` +
+      `treeIndex: ${treeIndex}`
+  );
+};
+
+const isVirtualized = true;
+const treeContainerStyle = isVirtualized ? { height: 450 } : {};
+
+export default class Demo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.updateTreeData = this.updateTreeData.bind(this);
+    this.expandAll = this.expandAll.bind(this);
+    this.collapseAll = this.collapseAll.bind(this);
+    this.selectNextMatch = this.selectNextMatch.bind(this);
+    this.selectPrevMatch = this.selectPrevMatch.bind(this);
+
+    this.state = {
+      searchString: '',
+      searchFocusIndex: 0,
+      searchFoundCount: null,
+      treeData: [
+        {
+          title: '`title`',
+          subtitle: '`subtitle`',
+          expanded: true,
+          children: [
+            {
+              title: 'Child Node',
+              subtitle: 'Defined in `children` array belonging to parent',
+            },
+            {
+              title: 'Nested structure is rendered virtually',
+              subtitle: (
+                <span>
+                  The tree uses&nbsp;
+                  <a href="https://github.com/bvaughn/react-virtualized">
+                    react-virtualized
+                  </a>
+                  &nbsp;and the relationship lines are more of a visual trick.
+                </span>
+              ),
+            },
+          ],
+        },
+        {
+          expanded: true,
+          title: 'Any node can be the parent or child of any other node',
+          children: [
+            {
+              expanded: true,
+              title: 'Chicken',
+              children: [{ title: 'Egg' }],
+            },
+          ],
+        },
+        {
+          title: 'Button(s) can be added to the node',
+          subtitle:
+            'Node info is passed when generating so you can use it in your onClick handler',
+        },
+        {
+          title: 'Show node children by setting `expanded`',
+          subtitle: ({ node }) =>
+            `expanded: ${node.expanded ? 'true' : 'false'}`,
+          children: [
+            {
+              title: 'Bruce',
+              subtitle: ({ node }) =>
+                `expanded: ${node.expanded ? 'true' : 'false'}`,
+              children: [{ title: 'Bruce Jr.' }, { title: 'Brucette' }],
+            },
+          ],
+        },
+        {
+          title: 'Advanced',
+          subtitle: 'Settings, behavior, etc.',
+          children: [
+            {
+              title: (
+                <div>
+                  <div
+                    style={{
+                      backgroundColor: 'gray',
+                      display: 'inline-block',
+                      borderRadius: 10,
+                      color: '#FFF',
+                      padding: '0 5px',
+                    }}
+                  >
+                    Any Component
+                  </div>
+                  &nbsp;can be used for `title`
+                </div>
+              ),
+            },
+            {
+              expanded: true,
+              title: 'Limit nesting with `maxDepth`',
+              subtitle: `It's set to ${maxDepth} for this example`,
+              children: [
+                {
+                  expanded: true,
+                  title: renderDepthTitle,
+                  children: [
+                    {
+                      expanded: true,
+                      title: renderDepthTitle,
+                      children: [
+                        { title: renderDepthTitle },
+                        {
+                          title: ({ path }) =>
+                            path.length >= maxDepth
+                              ? 'This cannot be dragged deeper'
+                              : 'This can be dragged deeper',
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              title:
+                'Disable dragging on a per-node basis with the `canDrag` prop',
+              subtitle: 'Or set it to false to disable all dragging.',
+              noDragging: true,
+            },
+            {
+              title: 'You cannot give this children',
+              subtitle:
+                'Dropping is prevented via the `canDrop` API using `nextParent`',
+              noChildren: true,
+            },
+            {
+              title:
+                'When node contents are really long, it will cause a horizontal scrollbar' +
+                ' to appear. Deeply nested elements will also trigger the scrollbar.',
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  updateTreeData(treeData) {
+    this.setState({ treeData });
+  }
+
+  expand(expanded) {
+    this.setState({
+      treeData: toggleExpandedForAll({
+        treeData: this.state.treeData,
+        expanded,
+      }),
+    });
+  }
+
+  expandAll() {
+    this.expand(true);
+  }
+
+  collapseAll() {
+    this.expand(false);
+  }
+
+  selectPrevMatch() {
+    const { searchFocusIndex, searchFoundCount } = this.state;
+
+    this.setState({
+      searchFocusIndex:
+        searchFocusIndex !== null
+          ? (searchFoundCount + searchFocusIndex - 1) % searchFoundCount
+          : searchFoundCount - 1,
+    });
+  }
+
+  selectNextMatch() {
+    const { searchFocusIndex, searchFoundCount } = this.state;
+
+    this.setState({
+      searchFocusIndex:
+        searchFocusIndex !== null
+          ? (searchFocusIndex + 1) % searchFoundCount
+          : 0,
+    });
+  }
+
+  render() {
+    const {
+      treeData,
+      searchString,
+      searchFocusIndex,
+      searchFoundCount,
+    } = this.state;
+
+    return (
+      <div style={treeContainerStyle}>
+        <SortableTree
+          treeData={treeData}
+          onChange={this.updateTreeData}
+          onMoveNode={({ node, treeIndex, path }) =>
+            global.console.debug(
+              'node:',
+              node,
+              'treeIndex:',
+              treeIndex,
+              'path:',
+              path
+            )
+          }
+          maxDepth={maxDepth}
+          searchQuery={searchString}
+          searchFocusOffset={searchFocusIndex}
+          canDrag={({ node }) => !node.noDragging}
+          canDrop={({ nextParent }) => !nextParent || !nextParent.noChildren}
+          searchFinishCallback={matches =>
+            this.setState({
+              searchFoundCount: matches.length,
+              searchFocusIndex:
+                matches.length > 0 ? searchFocusIndex % matches.length : 0,
+            })
+          }
+          isVirtualized={isVirtualized}
+          generateNodeProps={rowInfo => ({
+            buttons: [
+              <button
+                style={{
+                  verticalAlign: 'middle',
+                }}
+                onClick={() => alertNodeInfo(rowInfo)}
+              >
+                ℹ
+              </button>,
+            ],
+          })}
+        />
+      </div>
+    );
+  }
+}
