42c7e01d7831a25e474ffcf3948885545735dc1f
Wei-Wei Wu <wuxx1045@umn.edu>
2018-12-25 19:35:13 -0500

    doc: new website with codesandbox embed
    Website v3

---
+++ website/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import '../style.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="navigation-wrapper">
+          <a
+            className="title"
+            href="https://github.com/frontend-collective/react-sortable-tree"
+            taget="_blank"
+          >
+            React Sortable Tree
+          </a>
+          <a
+            className="github-button"
+            href="https://github.com/frontend-collective/react-sortable-tree"
+            data-icon="octicon-star"
+            data-size="large"
+            data-show-count="true"
+            aria-label="Star frontend-collective/react-sortable-tree on GitHub"
+          >
+            Star
+          </a>
+          <a className="link" href="./storybook" target="_blank">
+            View Storybook
+          </a>
+        </div>
+        <div className="description">
+          Drag-and-drop sortable component for nested data and hierarchies
+        </div>
+        <iframe
+          src="https://codesandbox.io/embed/xj7mmx93xz?odemirror=1&view=preview"
+          sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+        />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
---
+++ website/sandbox/App.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import SortableTree, { toggleExpandedForAll } from 'react-sortable-tree';
+
+import treeData from './treeData';
+
+const maxDepth = 5;
+
+const alertNodeInfo = ({ node, path, treeIndex }) => {
+  const objectString = Object.keys(node)
+    .map(k => (k === 'children' ? 'children: Array' : `${k}: '${node[k]}'`))
+    .join(',\n   ');
+
+  global.alert(
+    'Info passed to the button generator:\n\n' +
+      `node: {\n   ${objectString}\n},\n` +
+      `path: [${path.join(', ')}],\n` +
+      `treeIndex: ${treeIndex}`
+  );
+};
+
+export default class App extends React.Component {
+  state = {
+    searchString: '',
+    searchFocusIndex: 0,
+    searchFoundCount: null,
+    treeData,
+  };
+
+  handleTreeOnChange = treeData => {
+    this.setState({ treeData });
+  };
+
+  handleSearchOnChange = e => {
+    this.setState({
+      searchString: e.target.value,
+    });
+  };
+
+  selectPrevMatch = () => {
+    const { searchFocusIndex, searchFoundCount } = this.state;
+
+    this.setState({
+      searchFocusIndex:
+        searchFocusIndex !== null
+          ? (searchFoundCount + searchFocusIndex - 1) % searchFoundCount
+          : searchFoundCount - 1,
+    });
+  };
+
+  selectNextMatch = () => {
+    const { searchFocusIndex, searchFoundCount } = this.state;
+
+    this.setState({
+      searchFocusIndex:
+        searchFocusIndex !== null
+          ? (searchFocusIndex + 1) % searchFoundCount
+          : 0,
+    });
+  };
+
+  toggleNodeExpansion = expanded => {
+    this.setState(prevState => ({
+      treeData: toggleExpandedForAll({
+        treeData: prevState.treeData,
+        expanded,
+      }),
+    }));
+  };
+
+  render() {
+    const {
+      treeData,
+      searchString,
+      searchFocusIndex,
+      searchFoundCount,
+    } = this.state;
+
+    return (
+      <div className="wrapper">
+        <div className="bar-wrapper">
+          <button onClick={this.toggleNodeExpansion.bind(this, true)}>
+            Expand all
+          </button>
+          <button
+            className="collapse"
+            onClick={this.toggleNodeExpansion.bind(this, false)}
+          >
+            Collapse all
+          </button>
+          <label>Search: </label>
+          <input onChange={this.handleSearchOnChange} />
+          <button className="previous" onClick={this.selectPrevMatch}>
+            Previous
+          </button>
+          <button className="next" onClick={this.selectNextMatch}>
+            Next
+          </button>
+          <label>
+            {searchFocusIndex} / {searchFoundCount}
+          </label>
+        </div>
+        <div className="tree-wrapper">
+          <SortableTree
+            treeData={treeData}
+            onChange={this.handleTreeOnChange}
+            onMoveNode={({ node, treeIndex, path }) =>
+              global.console.debug(
+                'node:',
+                node,
+                'treeIndex:',
+                treeIndex,
+                'path:',
+                path
+              )
+            }
+            maxDepth={maxDepth}
+            searchQuery={searchString}
+            searchFocusOffset={searchFocusIndex}
+            canDrag={({ node }) => !node.noDragging}
+            canDrop={({ nextParent }) => !nextParent || !nextParent.noChildren}
+            searchFinishCallback={matches =>
+              this.setState({
+                searchFoundCount: matches.length,
+                searchFocusIndex:
+                  matches.length > 0 ? searchFocusIndex % matches.length : 0,
+              })
+            }
+            isVirtualized={true}
+            generateNodeProps={rowInfo => ({
+              buttons: [
+                <button
+                  className="btn btn-outline-success"
+                  style={{
+                    verticalAlign: 'middle',
+                  }}
+                  onClick={() => alertNodeInfo(rowInfo)}
+                >
+                  ℹ
+                </button>,
+              ],
+            })}
+          />
+        </div>
+      </div>
+    );
+  }
+}
