020e51c5b8eae77f5c53b4775e49f5666cb7e4d3
Yo Takezawa <takezawa.yo@tis.co.jp>
2019-05-15 14:46:53 +0900

    Add propTypes definition to pass latest eslint-plugin-react

--- src/react-sortable-tree.js
+++ src/react-sortable-tree.js
@@ -770,7 +770,9 @@ class ReactSortableTree extends Component {
 }
 
 ReactSortableTree.propTypes = {
-  dragDropManager: PropTypes.shape({}).isRequired,
+  dragDropManager: PropTypes.shape({
+    getMonitor: PropTypes.func,
+  }).isRequired,
 
   // Tree data in the following format:
   // [{title: 'main', subtitle: 'sub'}, { title: 'value2', expanded: true, children: [{ title: 'value3') }] }]
