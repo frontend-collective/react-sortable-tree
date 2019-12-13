c664adee1cc045a76a9f89c38b644aa996f38365
Wei-Wei Wu <wuxx1045@umn.edu>
2019-12-13 00:27:37 -0800

    fix: accidentally deleted own styling

--- website/sandbox/index.js
+++ website/sandbox/index.js
@@ -4,6 +4,7 @@ import App from './App';
 
 // eslint-disable-next-line import/no-unresolved
 import 'react-sortable-tree/style.css';
+import './styles.css';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
