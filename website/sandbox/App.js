69149596c884cb28c83c17f238c7d7d186271c44
Wei-Wei Wu <wuxx1045@umn.edu>
2019-12-13 00:24:20 -0800

    fix: website pt 2

--- website/sandbox/App.js
+++ website/sandbox/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line import/no-unresolved
 import SortableTree, { toggleExpandedForAll } from 'react-sortable-tree';
 
 import treeData from './treeData';
--- website/sandbox/index.js
+++ website/sandbox/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import './styles.css';
-import '../../src/react-sortable-tree.css';
-import '../../src/node-renderer-default.css';
-import '../../src/tree-node.css';
+// eslint-disable-next-line import/no-unresolved
+import 'react-sortable-tree/style.css';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
