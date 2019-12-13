95cb249e24fb8cab2134567f71447bd728228c1e
Wei-Wei Wu <wuxx1045@umn.edu>
2019-12-13 00:19:25 -0800

    fix: site

--- website/sandbox/App.js
+++ website/sandbox/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SortableTree, { toggleExpandedForAll } from '../../src';
+import SortableTree, { toggleExpandedForAll } from 'react-sortable-tree';
 
 import treeData from './treeData';
 
